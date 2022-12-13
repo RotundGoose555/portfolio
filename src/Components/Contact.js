5a69de2f31e3b19ef2ef4c5af68032160023b828
RotundGoose555 <115037018+RotundGoose555@users.noreply.github.com>
2022-12-13 18:16:37 -0500

    Add files via upload

---
+++ src/Components/Contact.js
@@ -0,0 +1,24 @@
+import '../CSS/Contact.css'
+function Contact() {
+    return(
+        <div className ='contact'>
+        
+        <div className='contact-box'>
+            <h3 className='contact-header'>Contact</h3>
+            <h2>PHONE</h2>
+            <h5>802-123-456</h5>
+            <br/>
+            <h2>EMAIL</h2>
+            <h5>john_doe@brown.edu</h5>
+            <br/>
+            <h2>LINKEDIN</h2>
+            <h5>JohnDoe</h5>
+            <br/>
+            <h2>INSTAGRAM</h2>
+            <h5>HeresJohnny</h5>
+        </div>
+        </div>
+    )
+    }
+
+export default Contact;
